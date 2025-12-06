ab6025f55267674ce8cfa9c856681aa44e8d8106
menacing132 <abouzahramikhael2@gmail.com>
2025-12-06 18:23:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function copyIP() {
+    navigator.clipboard.writeText("Coming Soon...");
+    alert("Server IP copied!");
+}
+
+async function getPlayers() {
+    try {
+        const res = await fetch("https://api.mcsrvstat.us/2/yourserverip.com");
+        const data = await res.json();
+        document.getElementById("playerCount").innerText = data.players ? data.players.online : 0;
+    } catch (err) {
+        console.log("Server offline or IP missing");
+    }
+}
+
+getPlayers();
+setInterval(getPlayers, 15000);
 
